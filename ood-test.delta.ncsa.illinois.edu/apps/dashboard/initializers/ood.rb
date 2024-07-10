892474e7de1cfe801e857bf880e0a529fb4f46fe
Yan Zhan <yanzhan2@illinois.edu>
2024-07-10 11:18:38 -0500

    add customizations to ood-test.delta

--- ood-test.delta.ncsa.illinois.edu/apps/dashboard/initializers/ood.rb
+++ ood-test.delta.ncsa.illinois.edu/apps/dashboard/initializers/ood.rb
@@ -1,7 +1,4 @@
 Rails.application.config.after_initialize do
   OodFilesApp.candidate_favorite_paths.tap do |paths|
-    paths << FavoritePath.new("/scratch")
-    paths << FavoritePath.new("/projects")
-    paths << FavoritePath.new("/taiga")
   end
 end
