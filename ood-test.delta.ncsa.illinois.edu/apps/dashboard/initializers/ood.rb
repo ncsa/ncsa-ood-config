2d2ba7fe27a53d1cc3054faaf89d990a67a1cc5e
Jake Rundall <rundall@illinois.edu>
2024-10-29 16:03:27 -0500

    SVCPLAN-5071: adjust FS paths for Delta complex instances
    add:
    /ime/ (Delta only)
    /old_scratch/ (Delta only)
    /work/
    
    remove:
    /scratch/ (DeltaAI only)

--- ood-test.delta.ncsa.illinois.edu/apps/dashboard/initializers/ood.rb
+++ ood-test.delta.ncsa.illinois.edu/apps/dashboard/initializers/ood.rb
@@ -1,7 +1,10 @@
 Rails.application.config.after_initialize do
   OodFilesApp.candidate_favorite_paths.tap do |paths|
-    paths << FavoritePath.new("/scratch")
+    paths << FavoritePath.new("/ime")
+    paths << FavoritePath.new("/old_scratch")
     paths << FavoritePath.new("/projects")
+    paths << FavoritePath.new("/scratch")
     paths << FavoritePath.new("/taiga")
+    paths << FavoritePath.new("/work")
   end
 end
