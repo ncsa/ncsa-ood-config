881ad296b891687b78b3462f2160b2e24bb2c3df
Jake Rundall <rundall@illinois.edu>
2024-11-18 12:36:12 -0600

    SVCPLAN-6608: remove /old_scratch/ mount

--- openondemand.delta.ncsa.illinois.edu/apps/dashboard/initializers/ood.rb
+++ openondemand.delta.ncsa.illinois.edu/apps/dashboard/initializers/ood.rb
@@ -1,7 +1,6 @@
 Rails.application.config.after_initialize do
   OodFilesApp.candidate_favorite_paths.tap do |paths|
     paths << FavoritePath.new("/ime")
-    paths << FavoritePath.new("/old_scratch")
     paths << FavoritePath.new("/projects")
     paths << FavoritePath.new("/scratch")
     paths << FavoritePath.new("/taiga")
